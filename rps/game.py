15e815ec692f769676fae66e37f268dd8f398520
ggorken <ggorken3578@floridapoly.edu>
2025-11-13 02:19:53 +0000

    Adding computer functionality

--- rps/game.py
+++ rps/game.py
@@ -1,1 +1,15 @@
+import random
+
 print("Welcome to Rock-Paper-Scissors!")
+
+choice = random.randint(1,3)
+computer = ""
+
+if choice == 1:
+    computer = "rock"
+elif choice == 2:
+    computer = "paper"
+elif choice == 3:
+    computer = "scissors"
+
+print(f"Computer chose: {computer}")
